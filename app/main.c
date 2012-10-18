ed74aaab236aa52c05632778f62b1f091da7fa62
Francesco Piccinno <stack.box@gmail.com>
2012-10-18 22:01:54 +0200

    Makefile update

--- app/main.c
+++ app/main.c
@@ -4,58 +4,50 @@
 #include "db.h"
 #include "sst_loader.h"
 
+#define ADD(x, y) \
+    buffer_clear(key); \
+    buffer_clear(value); \
+    buffer_putstr(key, x); \
+    buffer_putstr(value, y); \
+    db_add(db, key, value);
+
 int main(int argc, const char * argv[])
 {
+    int i;
     DB* db = db_open(START_DIRECTORY);
     Variant* key = buffer_new(256);
     Variant* value = buffer_new(256);
 
+    srand(time(NULL));
 
-#if 0
+    char skey[64];
 
-    size_t read;
-    size_t len;
-    char* line = NULL;
-
-    while ((read = getline(&line, &len, stdin)) != -1)
+    while (fscanf(stdin, "%s", skey) != EOF)
     {
-        if (key->length == 0)
-        {
-            buffer_clear(value);
-            buffer_putnstr(key, line, read - 1);
-        }
-        else if (value->length == 0)
-        {
-            buffer_putnstr(value, line, read - 1);
-            INFO("Key: %.*s Value: %.*s", key->length, key->mem, value->length, value->mem);
-
-            buffer_clear(key);
-            buffer_clear(value);
-        }
+      ADD(skey, skey);
     }
 
-    if (line)
-        free(line);
+    //ADD("V7", "vertice 0");
+    //ADD("V6", "vertice 1");
+    //ADD("E5", "edge 2");
 
-    //sst_compact(db->sst);
+#if 0
+    buffer_clear(key);
+    buffer_putstr(key, "V");
+    DBIterator* iter = db_iterator_new(db);
+    db_iterator_seek(iter, key);
 
-//    SSTLoaderIterator* iter = sst_loader_iterator((*db->sst->sorted_files)->loader);
-
-//    for (; sst_loader_iterator_valid(iter); sst_loader_iterator_next(iter))
-//        INFO("Key: %.*s", iter->key->length, iter->key->mem);
-
-#else
-
-#if 1
-#include "generated.c"
-#else
-    buffer_putnstr(key,   "Brittney", 8);
-    db_get(db, key, value);
-    if (memcmp(value->mem, "diomadonna", 10) != 0) { int3(); printf("ERMHAGHERD: ERROR: %.*s != %.*s\n", value->length, value->mem, 10, "diomadonna");}
+    while (db_iterator_valid(iter))
+    {
+        Variant* k = db_iterator_key(iter);
+        Variant* v = db_iterator_value(iter);
+        INFO("KEY: %.*s : %.*s\n", k->length, k->mem, v->length, v->mem);
+        db_iterator_next(iter);
+    }
 #endif
 
-#endif
     db_close(db);
+
     return 0;
 }
 
